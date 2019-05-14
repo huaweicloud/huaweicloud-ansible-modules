3d1d780460fa7987b3e22bebacb2f28ebed64acb
zengchen1024 <chenzeng2@huawei.com>
2019-05-14 15:54:23 +0800

    refactor search resource

--- library/hwc_vpc_subnet.py
+++ library/hwc_vpc_subnet.py
@@ -5,12 +5,10 @@
 # GNU General Public License v3.0+ (see COPYING or
 # https://www.gnu.org/licenses/gpl-3.0.txt)
 
-from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-
-###############################################################################
-# Documentation
-###############################################################################
+from ansible.module_utils.hwc_utils import (
+    Config, HwcClientException, HwcClientException404, HwcModule,
+    are_different_dicts, build_path, get_region, is_empty_value,
+    navigate_value, wait_to_finish)
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ["preview"],
@@ -33,6 +31,12 @@ options:
             - Whether the given object should exist in Huawei Cloud.
         choices: ['present', 'absent']
         default: 'present'
+    filters:
+        description:
+            - A list of filters to apply when deciding whether existing
+              resources match and should be altered. The item of filters
+              is the name of input options.
+        required: true
     timeouts:
         description:
             - The timeouts for each operations.
@@ -48,30 +52,12 @@ options:
                     - The timeouts for update operation.
                 type: str
                 default: '15m'
-    availability_zone:
-        description:
-            - Specifies the AZ to which the subnet belongs.
-        required: false
     cidr:
         description:
             - Specifies the subnet CIDR block. The value must be within the VPC
               CIDR block and be in CIDR format. The subnet mask cannot be
               greater than 28.
         required: true
-    dhcp_enable:
-        description:
-            - Specifies whether DHCP is enabled for the subnet. The value can
-              be true (enabled) or false(disabled), and default value is true.
-              If this parameter is set to false, newly created ECSs cannot
-              obtain IP addresses, and usernames and passwords cannot be
-              injected using Cloud-init.
-        required: false
-        type: bool
-    dns_address:
-        description:
-            - Specifies the DNS server addresses for subnet. Note: the address
-              in the head will be used first.
-        required: false
     gateway_ip:
         description:
             - Specifies the gateway of the subnet. The value must be an IP
@@ -87,6 +73,24 @@ options:
         description:
             - Specifies the ID of the VPC to which the subnet belongs.
         required: true
+    availability_zone:
+        description:
+            - Specifies the AZ to which the subnet belongs.
+        required: false
+    dhcp_enable:
+        description:
+            - Specifies whether DHCP is enabled for the subnet. The value can
+              be true (enabled) or false(disabled), and default value is true.
+              If this parameter is set to false, newly created ECSs cannot
+              obtain IP addresses, and usernames and passwords cannot be
+              injected using Cloud-init.
+        type: bool
+        required: false
+    dns_address:
+        description:
+            - Specifies the DNS server addresses for subnet. Note: the address
+              in the head will be used first.
+        required: false
 extends_documentation_fragment: hwc
 '''
 
@@ -107,18 +111,36 @@ EXAMPLES = '''
 '''
 
 RETURN = '''
-    availability_zone:
-        description:
-            - Specifies the AZ to which the subnet belongs.
-        returned: success
-        type: str
     cidr:
         description:
             - Specifies the subnet CIDR block. The value must be within the VPC
               CIDR block and be in CIDR format. The subnet mask cannot be
               greater than 28.
-        returned: success
         type: str
+        returned: success
+    gateway_ip:
+        description:
+            - Specifies the gateway of the subnet. The value must be an IP
+              address in the subnet.
+        type: str
+        returned: success
+    name:
+        description:
+            - Specifies the subnet name. The value is a string of 1 to 64
+              characters that can contain letters, digits, underscores C(_),
+              hyphens (-), and periods (.).
+        type: str
+        returned: success
+    vpc_id:
+        description:
+            - Specifies the ID of the VPC to which the subnet belongs.
+        type: str
+        returned: success
+    availability_zone:
+        description:
+            - Specifies the AZ to which the subnet belongs.
+        type: str
+        returned: success
     dhcp_enable:
         description:
             - Specifies whether DHCP is enabled for the subnet. The value can
@@ -126,76 +148,61 @@ RETURN = '''
               If this parameter is set to false, newly created ECSs cannot
               obtain IP addresses, and usernames and passwords cannot be
               injected using Cloud-init.
-        returned: success
         type: bool
+        returned: success
     dns_address:
         description:
             - Specifies the DNS server addresses for subnet. Note: the address
               in the head will be used first.
-        returned: success
         type: list
-    gateway_ip:
-        description:
-            - Specifies the gateway of the subnet. The value must be an IP
-              address in the subnet.
         returned: success
-        type: str
-    name:
-        description:
-            - Specifies the subnet name. The value is a string of 1 to 64
-              characters that can contain letters, digits, underscores C(_),
-              hyphens (-), and periods (.).
-        returned: success
-        type: str
-    vpc_id:
-        description:
-            - Specifies the ID of the VPC to which the subnet belongs.
-        returned: success
-        type: str
 '''
 
-###############################################################################
-# Imports
-###############################################################################
 
-from ansible.module_utils.hwc_utils import (Config, HwcClientException,
-                                            HwcClientException404, HwcModule,
-                                            are_different_dicts, get_region,
-                                            is_empty_value, build_path,
-                                            navigate_value, wait_to_finish)
-
-###############################################################################
-# Main
-###############################################################################
+def build_module():
+    return HwcModule(
+        argument_spec=dict(
+            state=dict(default='present', choices=['present', 'absent'],
+                       type='str'),
+            filters=dict(required=True, type='list', elements='str'),
+            timeouts=dict(type='dict', options=dict(
+                create=dict(default='15m', type='str'),
+                update=dict(default='15m', type='str'),
+            ), default=dict()),
+            cidr=dict(required=True, type='str'),
+            gateway_ip=dict(required=True, type='str'),
+            name=dict(required=True, type='str'),
+            vpc_id=dict(required=True, type='str'),
+            availability_zone=dict(type='str'),
+            dhcp_enable=dict(type='bool'),
+            dns_address=dict(type='list', elements='str')
+        ),
+        supports_check_mode=True,
+    )
 
 
 def main():
     """Main function"""
 
-    module = HwcModule(
-        argument_spec=dict(
-            state=dict(default='present', choices=['present', 'absent'],
-                       type='str'),
-            timeouts=dict(type='dict', options=dict(
-                create=dict(default='15m', type='str'),
-                update=dict(default='15m', type='str'),
-            ), default=dict()),
-            availability_zone=dict(type='str'),
-            cidr=dict(required=True, type='str'),
-            dhcp_enable=dict(type='bool'),
-            dns_address=dict(type='list', elements='str'),
-            gateway_ip=dict(required=True, type='str'),
-            name=dict(required=True, type='str'),
-            vpc_id=dict(required=True, type='str')
-        ),
-        supports_check_mode=True,
-    )
-
+    module = build_module()
     config = Config(module, "vpc")
+
     try:
-        resource = fetch_resource(config)
-        if resource:
-            module.params['id'] = navigate_value(resource, ["id"])
+        resource = None
+        if module.params['id']:
+            resource = True
+        else:
+            v = search_resource(config)
+            if len(v) > 1:
+                raise Exception(
+                    "find more than one resources(%s)" % ", ".join([
+                            navigate_value(i, ["id"])
+                            for i in v
+                        ]))
+
+            if len(v) == 1:
+                resource = v[0]
+                module.params['id'] = navigate_value(resource, ["id"])
 
         result = {}
         changed = False
@@ -304,39 +311,47 @@ def read_resource(config, exclude_output=False):
 
     res = {}
     r = send_read_request(module, client)
-    fill_read_resp_body(r)
-    res["read"] = r
+    res["read"] = fill_read_resp_body(r)
 
     return update_properties(module, res, exclude_output)
 
 
-def fetch_resource(config):
+def _build_query_link(opts):
+    query_link = "?marker={marker}&limit=10"
+    v = navigate_value(opts, ["vpc_id"])
+    if v:
+        query_link += "&vpc_id=" + str(v)
+
+    return query_link
+
+
+def search_resource(config):
     module = config.module
     client = config.client(get_region(module), "vpc", "project")
     opts = user_input_parameters(module)
-    identity = {
-        "vpc_id": navigate_value(opts, ["vpc_id"]),
-        "name": navigate_value(opts, ["name"]),
-    }
+    identity_obj = _build_identity_object(module, opts)
+    query_link = _build_query_link(opts)
+    link = "subnets" + query_link
 
-    link = "subnets"
-    query_link = "?marker={marker}&limit=10&vpc_id=" + str(identity["vpc_id"])
-    link += query_link
-
+    result = []
     p = {'marker': ''}
     while True:
         url = link.format(**p)
         r = send_list_request(module, client, url)
         if not r:
-            return None
+            break
+
         for item in r:
-            for k, v in identity.items():
-                if item[k] != v:
-                    break
-            else:
-                return item
+            item = fill_list_resp_body(item)
+            if not are_different_dicts(identity_obj, item):
+                result.append(item)
+
+        if len(result) > 1:
+            break
 
         p['marker'] = r[-1].get('id')
+
+    return result
 
 
 def build_create_parameters(opts):
@@ -458,7 +473,7 @@ def build_update_parameters(opts):
         params["dhcp_enable"] = v
 
     v = expand_update_dns_list(opts, None)
-    if not is_empty_value(v):
+    if v is not None:
         params["dnsList"] = v
 
     v = navigate_value(opts, ["name"], None)
@@ -466,11 +481,11 @@ def build_update_parameters(opts):
         params["name"] = v
 
     v = expand_update_primary_dns(opts, None)
-    if not is_empty_value(v):
+    if v is not None:
         params["primary_dns"] = v
 
     v = expand_update_secondary_dns(opts, None)
-    if not is_empty_value(v):
+    if v is not None:
         params["secondary_dns"] = v
 
     if not params:
@@ -483,7 +498,11 @@ def build_update_parameters(opts):
 
 def expand_update_dns_list(d, array_index):
     v = navigate_value(d, ["dns_address"], array_index)
-    return v if (v and len(v) > 2) else []
+    if v:
+        if len(v) > 2:
+            return v
+        return None
+    return []
 
 
 def expand_update_primary_dns(d, array_index):
@@ -574,32 +593,35 @@ def send_read_request(module, client):
 
 
 def fill_read_resp_body(body):
+    result = dict()
 
-    body.setdefault("availability_zone", None)
+    result["availability_zone"] = body.get("availability_zone")
 
-    body.setdefault("cidr", None)
+    result["cidr"] = body.get("cidr")
 
-    body.setdefault("dhcp_enable", None)
+    result["dhcp_enable"] = body.get("dhcp_enable")
 
-    body.setdefault("dnsList", None)
+    result["dnsList"] = body.get("dnsList")
 
-    body.setdefault("gateway_ip", None)
+    result["gateway_ip"] = body.get("gateway_ip")
 
-    body.setdefault("id", None)
+    result["id"] = body.get("id")
 
-    body.setdefault("name", None)
+    result["name"] = body.get("name")
 
-    body.setdefault("neutron_network_id", None)
+    result["neutron_network_id"] = body.get("neutron_network_id")
 
-    body.setdefault("neutron_subnet_id", None)
+    result["neutron_subnet_id"] = body.get("neutron_subnet_id")
 
-    body.setdefault("primary_dns", None)
+    result["primary_dns"] = body.get("primary_dns")
 
-    body.setdefault("secondary_dns", None)
+    result["secondary_dns"] = body.get("secondary_dns")
 
-    body.setdefault("status", None)
+    result["status"] = body.get("status")
 
-    body.setdefault("vpc_id", None)
+    result["vpc_id"] = body.get("vpc_id")
+
+    return result
 
 
 def update_properties(module, response, exclude_output=False):
@@ -640,6 +662,82 @@ def send_list_request(module, client, url):
         module.fail_json(msg=msg)
 
     return navigate_value(r, ["subnets"], None)
+
+
+def _build_identity_object(module, all_opts):
+    filters = module.params.get("filters")
+    opts = dict()
+    for k, v in all_opts.items():
+        opts[k] = v if k in filters else None
+
+    result = dict()
+
+    v = navigate_value(opts, ["availability_zone"], None)
+    result["availability_zone"] = v
+
+    v = navigate_value(opts, ["cidr"], None)
+    result["cidr"] = v
+
+    v = navigate_value(opts, ["dhcp_enable"], None)
+    result["dhcp_enable"] = v
+
+    v = navigate_value(opts, ["dns_address"], None)
+    result["dnsList"] = v
+
+    v = navigate_value(opts, ["gateway_ip"], None)
+    result["gateway_ip"] = v
+
+    result["id"] = None
+
+    v = navigate_value(opts, ["name"], None)
+    result["name"] = v
+
+    result["neutron_network_id"] = None
+
+    result["neutron_subnet_id"] = None
+
+    result["primary_dns"] = None
+
+    result["secondary_dns"] = None
+
+    result["status"] = None
+
+    v = navigate_value(opts, ["vpc_id"], None)
+    result["vpc_id"] = v
+
+    return result
+
+
+def fill_list_resp_body(body):
+    result = dict()
+
+    result["availability_zone"] = body.get("availability_zone")
+
+    result["cidr"] = body.get("cidr")
+
+    result["dhcp_enable"] = body.get("dhcp_enable")
+
+    result["dnsList"] = body.get("dnsList")
+
+    result["gateway_ip"] = body.get("gateway_ip")
+
+    result["id"] = body.get("id")
+
+    result["name"] = body.get("name")
+
+    result["neutron_network_id"] = body.get("neutron_network_id")
+
+    result["neutron_subnet_id"] = body.get("neutron_subnet_id")
+
+    result["primary_dns"] = body.get("primary_dns")
+
+    result["secondary_dns"] = body.get("secondary_dns")
+
+    result["status"] = body.get("status")
+
+    result["vpc_id"] = body.get("vpc_id")
+
+    return result
 
 
 if __name__ == '__main__':
